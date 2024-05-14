61ed2f7b6d0c257ecba4db380806dd2f507fb6a6
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-05-13 23:00:29 -0300

    tela inicial com listagem de buffets

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 const app = Vue.createApp({
-  // data() {
-  //   return {}
-  // },
-  // computed: {},
-  // async mounted() { },
-  // methods: {}
+  data() {
+    return {
+      listBuffets: []
+    }
+  },
+  computed: {},
+  async mounted() {
+    await this.getBuffets();
+  },
+  methods: {
+    getBuffets: async function () {
+      let response = await fetch('http://localhost:3000/api/v1/buffet');
+      let data = await response.json();
+
+      console.log(data)
+
+      if (data) {
+        data.forEach(item => {
+          const buffet = new Object();
+          buffet.commercial_name = item.commercial_name;
+          this.listBuffets.push(buffet);
+        });
+      } else {
+        console.error("Nenhum resultado encontrado no data");
+      }
+    }
+  }
 
 })
 
